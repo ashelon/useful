2ab3586d2b4ad498cdb1004d6fe0835887146975
Wouter Haesaerts <wouter@inuits.eu>
2015-04-01 11:57:09 +0200

    Drupal 7 module with frequently used functions

---
+++ MODULE/templates/name_of_template_file.tpl.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ array $items has been fed here through the theme() function in create_block.inc.
+
+*/
+
+  //dpm($variables, 'array of variables from create_block.inc');  
+
+
+?>
+
+<div class="variables">
+
+  <?php 
+    foreach ($variables as $key => $value) {
+      // action.
+    }
+
+  ?>
+
+</div>
